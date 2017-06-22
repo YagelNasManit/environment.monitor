33f152276d4b74ee67a858d3cef14853f7129e00
YagelNasManit <YagelNasManit@gmail.com>
2017-06-22 17:13:41 +0300

    added possibility to pass mongo client connection URI as system variable

--- environment.monitor.runner/src/main/java/org/yagel/monitor/mongo/MongoConnector.java
+++ environment.monitor.runner/src/main/java/org/yagel/monitor/mongo/MongoConnector.java
@@ -1,6 +1,7 @@
 package org.yagel.monitor.mongo;
 
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoDatabase;
 import org.apache.log4j.Logger;
 import org.yagel.monitor.exception.DiagnosticException;
@@ -21,10 +22,24 @@ public class MongoConnector {
 
   }
 
+  private MongoConnector(String connectURIStr) throws DiagnosticException {
+    MongoClientURI clientURI = new MongoClientURI(connectURIStr);
+    client = new MongoClient(clientURI);
+    String dbName = clientURI.getDatabase() == null ? MONITOR_DB : clientURI.getDatabase();
+    db = client.getDatabase(dbName);
+
+  }
+
   public static MongoConnector getInstance() {
     if (connector == null) {
       try {
-        connector = new MongoConnector();
+        String mongoConnectURI = System.getProperty("mongo.connect.uri", null);
+
+        if (mongoConnectURI == null)
+          connector = new MongoConnector();
+        else
+          connector = new MongoConnector(mongoConnectURI);
+
       } catch (Exception e) {
         log.error("Exception on mongoDB connection creation. ", e);
         throw new RuntimeException(e);
