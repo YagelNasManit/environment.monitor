d99f11e441060b4ac4571e44300a3bd7d11eae1a
YagelNasManit <YagelNasManit@gmail.com>
2017-12-20 13:32:48 +0200

    unit  coverage

--- environment.monitor.runner/src/test/java/org/yagel/monitor/runner/test/unit/DataUtilsTest.java
+++ environment.monitor.runner/src/test/java/org/yagel/monitor/runner/test/unit/DataUtilsTest.java
@@ -8,14 +8,30 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
 public class DataUtilsTest {
 
   @Test
-  public void testJoinYearAndMonthFirstMonthOfYear() throws Exception {
+  public void testDateConversion() {
+    Date date = new Date();
+    LocalDate localDate = DataUtils.asLocalDate(date);
 
+    Calendar cal = Calendar.getInstance();
+    cal.setTime(date);
+
+    Assert.assertEquals(cal.get(Calendar.YEAR), localDate.getYear());
+    // as in calendar month indexes start from 0
+    Assert.assertEquals(cal.get(Calendar.MONTH) + 1, localDate.getMonth().getValue());
+    Assert.assertEquals(cal.get(Calendar.DAY_OF_MONTH), localDate.getDayOfMonth());
+
+
+  }
+
+  @Test
+  public void testJoinYearAndMonthFirstMonthOfYear() throws Exception {
     LocalDate localDateTime = LocalDate.of(2017, 1, 1);
     int joinedYearAndMonth = DataUtils.joinYearMonthValues(DataUtils.asDate(localDateTime));
     Assert.assertEquals(joinedYearAndMonth, 201700);
@@ -32,8 +48,7 @@ public class DataUtilsTest {
 
   @Test
   public void testYesterdayCalculation() throws Exception {
-
-    Date currentDate = DataUtils.asDate(LocalDateTime.of(2017,10,10,10,10));
+    Date currentDate = DataUtils.asDate(LocalDateTime.of(2017, 10, 10, 10, 10));
 
     Date date = DataUtils.getYesterday(currentDate);
 
@@ -89,6 +104,15 @@ public class DataUtilsTest {
 
     Assert.assertEquals(monthFrames.get(0)[0], DataUtils.asDate(start));
     Assert.assertEquals(monthFrames.get(monthFrames.size() - 1)[1], DataUtils.asDate(end));
+  }
+
+  @Test(expectedExceptions = IllegalArgumentException.class)
+  public void testSplitDatesIntoMonthesInvalidParams(){
+    LocalDate end = LocalDate.of(2017, 12, 3);
+    Date endDate = DataUtils.asDate(end);
+
+    DataUtils.splitDatesIntoMonths(endDate,endDate);
+
   }
 
 }
